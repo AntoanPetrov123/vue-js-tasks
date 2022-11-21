935b8cf51a7e50f2856a09e9611e8a678d8f50d4
Antoan Petrov <antoan999_kn@abv.bg>
2022-11-21 21:31:43 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+export default {
+    data: function () {
+        return {
+            task: [
+                'Just task to do!'
+            ],
+            maxTasks: 10
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
