988f11ccedeabcddc3e1116f299ba8113db0ace4
Antoan Petrov <antoan999_kn@abv.bg>
2022-11-21 22:03:09 +0200

    task look

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
     <div class="container">
-
+        <app-task-grid :tasks="tasks"></app-task-grid>
     </div>
 </template>
 
 <script>
+import TaskGrid from './components/TaskGrid.vue'
+
 export default {
     data: function () {
         return {
-            task: [
+            tasks: [
                 'Just task to do!'
             ],
             maxTasks: 10
         }
+    },
+    components: {
+        appTaskGrid: TaskGrid
     }
 }
 </script>
